98296690c962a90713238f29d4092bef2c875129
Gabriel Gomes <gasigo10@gmail.com>
2017-05-16 08:03:08 -0300

    Projeto iOS criado

---
+++ Front-End/Rio Hospital/Rio HospitalTests/Rio_HospitalTests.swift
@@ -0,0 +1,36 @@
+//
+//  Rio_HospitalTests.swift
+//  Rio HospitalTests
+//
+//  Created by Gabriel Gomes on 16/05/17.
+//  Copyright © 2017 Gabriel Gomes. All rights reserved.
+//
+
+import XCTest
+@testable import Rio_Hospital
+
+class Rio_HospitalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
